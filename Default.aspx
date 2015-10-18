47a9a78b23af192188a357dbb71fbf9fee52b60c
Rodolfo Gallegos <rodolfo@one-team.cl>
2015-10-18 14:20:14 -0300

    AgenteNomade Hasta el 18/10/2015

---
+++ Default.aspx
@@ -0,0 +1,167 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <title>AGENTE NOMADE - Buscadir de Ofertas Inmobiliarias / Compra - Vende - Arrienda</title>
+
+    <meta name="description" content="Source code generated using layoutit.com"/>
+    <meta name="author" content="LayoutIt!"/>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"/>
+
+  </head>
+  <body>
+  <form runat="server">
+    <%--Barra superior--%>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbarCollapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><asp:Image runat="server" ID="Image1" ImageUrl="~/img/logo_h.png" /></a>
+				    </div>
+				    <div class="collapse navbar-collapse navbarCollapse">
+					    <ul class="nav navbar-nav navbar-right1">
+						    <li>
+							    <button class="btn btn-success" type="button">Publicar</button>&nbsp;&nbsp;
+						    </li>
+						    <li class="dropdown">
+                                <div class="btn-group">
+                                    <a class="btn btn-primary" href="#"><i class="icon-user icon-white"></i> Nosotros</a>
+                                    <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>&nbsp;&nbsp;
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Quienes Somos</a></li>
+                                        <li><a href="#">Vision</a></li>
+	                                    <li><a href="#">Mision</a></li>
+                                        <li><a href="#">Estructura</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Contactenos</a></li>
+                                    </ul>
+                                </div>
+						    </li>
+					    </ul>
+				    </div>
+			    </nav>
+
+    <%--Pagina Principal--%>
+    <div class="container-fluid">
+	    <div class="row2">
+		    <div class="col-md-12">
+			    <div class="page-header">
+                    <h3><small>Buscador de Ofertas Inmobiliarias</small><br />Compra / Vende / Arrienda</h3>
+			    </div>
+		    </div>
+	    </div>
+        <div class="row3">
+            <div class="col-md-2">
+                <div class="espacio_chico">
+                    <p><asp:Label ID="label_titulo" CssClass="titulo-centrado" runat="server" Text="CUADRO DE BUSQUEDA" Font-Bold="true" /></p>
+                    <p><asp:Label ID="label_negocio" runat="server" Text="OPERACIÓN" Font-Size="X-Small" /></p>
+                    <p><asp:Label ID="req_negocio" runat="server" Text="REQUERIDO" Font-Bold="true" Font-Size="X-Small" Font-Names="ARIAL" ForeColor="White" BackColor="Red" Visible="false" CssClass="text-center label label-default"  /></p>
+                    <p><asp:DropDownList id="negocio" runat="server" CssClass="btn btn-warning" Width="100%" AutoPostBack="true" OnSelectedIndexChanged="negocio_SelectedIndexChanged" /></p>
+                    <p><asp:Label ID="label_tipo" runat="server" Text="TIPO DE PROPIEDAD" Font-Size="X-Small" /></p>
+                    <p><asp:Label ID="req_tipo" runat="server" Text="REQUERIDO" Font-Bold="true" Font-Size="X-Small" Font-Names="ARIAL" ForeColor="White" BackColor="Red" Visible="false" CssClass="text-center label label-default"  /></p>
+                    <p><asp:DropDownList id="tipo" runat="server" CssClass="btn btn-primary" Width="100%" Enabled="false" AutoPostBack="true" OnSelectedIndexChanged="tipo_SelectedIndexChanged" /></p>
+                    <p><asp:Label ID="label_region" runat="server" Text="REGIÓN" Font-Size="X-Small" /></p>
+                    <p><asp:Label ID="req_region" runat="server" Text="REQUERIDO" Font-Bold="true" Font-Size="X-Small" Font-Names="ARIAL" ForeColor="White" BackColor="Red" Visible="false" CssClass="text-center label label-default"  /></p>
+                    <p><asp:DropDownList id="region" runat="server" CssClass="btn btn-primary" Width="100%" AutoPostBack="true" OnSelectedIndexChanged="region_SelectedIndexChanged" Enabled="false" /></p>
+                    <p><asp:Label ID="label_ciudad" runat="server" Text="CIUDAD/COMUNA" Font-Size="X-Small" /></p>
+                    <p><asp:Label ID="rep_region" runat="server" Text="REQUERIDO" Font-Bold="true" Font-Size="X-Small" Font-Names="ARIAL" ForeColor="White" BackColor="Red" Visible="false" CssClass="text-center label label-default"  /></p>
+                    <p><asp:DropDownList id="ciudad" runat="server" CssClass="btn btn-primary" Width="100%" Enabled="false" AutoPostBack="true" OnSelectedIndexChanged="ciudad_SelectedIndexChanged">
+                        <asp:ListItem Text="(Seleccione)" Value="0" />
+                    </asp:DropDownList></p>
+                    <table class="col-md-12">
+                        <tr>
+                            <td class="text-left">
+                                <p><asp:Label ID="label_monto_desde" runat="server" Text="DESDE ($)" Font-Size="X-Small" /></p>
+                                <p><asp:TextBox ID="desde" runat="server" Text="Desde" Width="90px" Enabled="false" TextMode="Number" /></p>
+                            </td>
+                            <td class="text-right">
+                                <p><asp:Label ID="label_monto_hasta" runat="server" Text="HASTA ($)" Font-Size="X-Small" /></p>
+                                <p><asp:TextBox ID="hasta" runat="server" Text="Hasta" Width="90px" Enabled="false" TextMode="Number" /></p>
+                            </td>
+                        </tr>
+                    </table>
+                    <br />
+                    <asp:Button ID="buscar_btn" runat="server" Text="Buscar" Width="100%" Height="22px" OnClick="buscar_btn_Click"/>
+                    <asp:Label ID="label_botton" runat="server" Text="" CssClass="text-center label label-default" Width="100%" Font-Bold="true" Font-Size="X-Small" Font-Names="ARIAL" ForeColor="White" BackColor="Red" Visible="false" />
+                    <p>&nbsp;</p>
+                </div>
+                <p>&nbsp;</p>
+		    </div><%--columna de busqueda--%>
+		    <div class="col-md-10">
+                <div class="row3">
+                    <asp:DataList ID="DataList1" runat="server" DataKeyField="codigo" Width="100%" ShowHeader="true" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="0px" CellPadding="3" ShowFooter="False" HeaderStyle-Height="35px" HeaderStyle-HorizontalAlign="left">
+                    <FooterStyle BackColor="White" ForeColor="#000066" />
+                    <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" CssClass="modal-backdrop.in"/>
+                    <HeaderTemplate>&nbsp;&nbsp;Resultados de su Busqueda:&nbsp;</HeaderTemplate>
+                    <ItemStyle ForeColor="#000066" CssClass="linea-abajo"/>
+                    <ItemTemplate>
+                        <div class="row-detalle center">
+                            <div class="col-sm-3">
+                                <img alt="" class="img-detalle" src="/img_prop/<%# Eval("codigo") %>.jpg" />
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p><asp:Label ID="Label5" CssClass="label label-info" runat="server" Text='<%# Eval("estado_str") %>' />&nbsp;<asp:Label ID="Label6" runat="server" CssClass="label label-danger" Text='<%# Eval("nom_tipo") %>' /></p>
+                                        <h4><asp:Label ID="Label7" runat="server" Font-Bold="true" Text='<%# Eval("calle") %>' />&nbsp;<asp:Label ID="Label8" runat="server" Font-Bold="true" Text='<%# Eval("numero") %>' />,&nbsp;<asp:Label ID="Label9" runat="server"  Font-Bold="true"  Text='<%# Eval("comuna_str") %>' /></h4>
+                                        <p><asp:Label ID="Label10" runat="server" Text='<%# Eval("villa_condominio") %>' /></p>
+                                        <p><span class="label label-primary"><asp:Label ID="Label11" runat="server" Text='<%# Eval("habitaciones") %>' /> Habitacion(es)</span>&nbsp;<span class="label label-success"><asp:Label ID="Label12" runat="server" Text='<%# Eval("banos") %>' /> Baño(s)</span>&nbsp;<span class="label label-default"><asp:Label ID="Label13" runat="server" Text='<%# Eval("estacionamiento") %>' /> Estacionamiento(s)</span></p>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <p><asp:Button runat="server" ID="btn_ver" CssClass="btn btn-danger" Text="Ver" />&nbsp;<asp:Button runat="server" ID="btn_guardar" CssClass="btn btn-warning" Text="Seleccionar" /></p>
+                                        <p><span class="glyphicon glyphicon-user" aria-hidden="true" />&nbsp;<span class="glyphicon glyphicon-earphone" aria-hidden="true" />&nbsp;<span class="glyphicon glyphicon-phone-alt" aria-hidden="true" />&nbsp;<span class="glyphicon glyphicon-envelope" aria-hidden="true" /></p>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <p class="letra-chica">VALOR ($):</p>
+                                        <p><asp:Label ID="Label14" runat="server" width="90px" Font-Bold="true" Font-Size="Medium" Text='<%# Eval("valor_peso", "{0:0,000}") %>' /></p>
+                                        <p class="letra-chica">VALOR (UF):</p>
+                                        <p><asp:Label ID="Label4" runat="server" Font-Bold="true" Font-Size="Medium" Width="80px" Text='<%# Eval("valor_uf", "{0:0,000}") %>' /></p>
+                                        <p class="letra-chica">CODIGO: <asp:Label ID="Label15" Width="80px" runat="server" Text='<%# Eval("codigo") %>' /></p>
+                                    </div>
+                                </div>            
+                            </div>    
+                        </div>
+                    </ItemTemplate>
+                    <SelectedItemStyle Font-Names="Sans Serif" BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                </asp:DataList>
+                </div>
+		    </div><%--pagina de resultados--%>
+        </div>
+    </div>
+
+    <div class="clearfix" />
+
+    <%--Barra Inferior--%>
+    <nav class="navbar navbar-default" role="banner" >
+        <div class="form-group">
+            <table class="col-md-12">
+                <tr>
+                    <td class="col-md-6 text-left">
+						<asp:Label runat="server" ID="Label_Suscribete" Text="SUSCRIBA SU EMAIL" Font-Bold="true" /><br /><asp:Label runat="server" ID="Label1" Text="Reciba Ofertas Inmobiliarias seleccionadas para Usted." Font-Size="small" /><br />
+                        <asp:TextBox runat="server" ID="Mail_Suscripcion" />&nbsp;&nbsp;<asp:Button runat="server" ID="Suscribir" Text="Suscribirse" />
+                    </td>
+                    <td class="col-md-6 text-right">
+                        <address><br /><strong>AGENTE NOMADE</strong><br/> Marin 227, Providencia<br /> Santiago, Chile<br /> CP 7501337<br/> <abbr title="Phone"></abbr></address>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </nav>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </form>
+  </body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    String ConnectionString = "server=127.0.0.1;uid=root;pwd=401240;database=portal;";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            using (var conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+                /* LLENA EL DROPBOXLIST OPERACION */
+                using (var cmd = new MySqlCommand("select id_negocio, nom_negocio from negocio", conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            negocio.DataSource = reader;
+                            negocio.DataValueField = "id_negocio";
+                            negocio.DataTextField = "nom_negocio";
+                            negocio.DataBind();
+                        }
+                    }
+                }
+                /* LLENA EL DROPBOXLIST TIPO DE PROPIEDAD */
+                using (var cmd1 = new MySqlCommand("select id_tipo, nom_tipo from tipo", conn))
+                {
+                    using (var reader1 = cmd1.ExecuteReader())
+                    {
+                        if (reader1.HasRows)
+                        {
+                            tipo.DataSource = reader1;
+                            tipo.DataValueField = "id_tipo";
+                            tipo.DataTextField = "nom_tipo";
+                            tipo.DataBind();
+                        }
+                    }
+                }
+                /* LLENA EL DROPBOXLIST REGION */
+                using (var cmd2 = new MySqlCommand("select id_re, str_descripcion from region_cl", conn))
+                {
+                    using (var reader2 = cmd2.ExecuteReader())
+                    {
+                        if (reader2.HasRows)
+                        {
+                            region.DataSource = reader2;
+                            region.DataValueField = "id_re";
+                            region.DataTextField = "str_descripcion";
+                            region.DataBind();
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+    }
+
+    protected void region_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        using (var conn = new MySqlConnection(ConnectionString))
+        {
+            ciudad.Enabled = true;
+            conn.Open();
+            /* LLENA EL DROPBOXLIST COMUNA */
+            string otro = region.Items[region.SelectedIndex].Value;
+            string union = "SELECT comuna_cl.id_co, comuna_cl.str_descripcion FROM region_cl LEFT JOIN (provincia_cl LEFT JOIN comuna_cl ON provincia_cl.id_pr = comuna_cl.id_pr) ON region_cl.id_re = provincia_cl.id_re WHERE ((region_cl.id_re)=" + otro + ")";
+            using (var cmd4 = new MySqlCommand(union, conn))
+            {
+                using (var reader4 = cmd4.ExecuteReader())
+                {
+                    if (reader4.HasRows)
+                    {
+                        ciudad.DataSource = reader4;
+                        ciudad.DataValueField = "id_co";
+                        ciudad.DataTextField = "str_descripcion";
+                        ciudad.DataBind();
+                    }
+                }
+            }
+            conn.Close();
+        }
+    }
+
+    protected void negocio_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        tipo.Enabled = true;
+    }
+
+    protected void tipo_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        region.Enabled = true;
+    }
+
+    protected void ciudad_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        desde.Enabled = true;
+        hasta.Enabled = true;
+    }
+
+    protected void Nueva_tabla(String neg, String tip, String reg, String ciu, double de, double ha)
+    {
+
+        MySqlConnection objConn = new MySqlConnection(ConnectionString);
+        objConn.Open();
+
+        /*String strSQL = "SELECT propiedades.codigo, negocio.nom_negocio, tipo.nom_tipo, region_cl.str_descripcion, comuna_cl.str_descripcion as comuna_str, estado.str_descripcion as estado_str, propiedades.calle, propiedades.numero, propiedades.villa_condominio, propiedades.habitaciones, propiedades.banos, propiedades.estacionamiento, propiedades.valor_peso, propiedades.valor_uf, tipo_tramitante.nombre_tipo_tramitante, propiedades.fecha, propiedades.vigente FROM((((((propiedades LEFT JOIN comuna_cl ON propiedades.comuna_ciudad = comuna_cl.id_co) LEFT JOIN negocio ON propiedades.operacion = negocio.id_negocio) LEFT JOIN tipo ON propiedades.tipo = tipo.id_tipo) LEFT JOIN region_cl ON propiedades.region = region_cl.id_re) LEFT JOIN estado ON propiedades.estado = estado.id_estado) LEFT JOIN tramitante ON propiedades.tramitante = tramitante.id_tramitante) LEFT JOIN tipo_tramitante ON tramitante.tipo_tramitante = tipo_tramitante.id_tipo_tramitante WHERE(((negocio.nom_negocio) = 'ARRENDAR') AND((tipo.nom_tipo) = 'CASA') AND((region_cl.str_descripcion) = 'METROPOLITANA DE SANTIAGO') AND((comuna_cl.str_descripcion) = 'MAIPÚ'))";*/
+
+        if (ha == 0)
+        {
+            if (reg != "0")
+            {
+                if (ciu != "0")
+                {
+                    String strSQL = String.Format("SELECT propiedades.codigo, negocio.nom_negocio, tipo.nom_tipo, region_cl.str_descripcion, comuna_cl.str_descripcion as comuna_str, estado.str_descripcion as estado_str, propiedades.calle, propiedades.numero, propiedades.villa_condominio, propiedades.habitaciones, propiedades.banos, propiedades.estacionamiento, propiedades.valor_peso, propiedades.valor_uf, tipo_tramitante.nombre_tipo_tramitante, propiedades.fecha, propiedades.vigente FROM((((((propiedades LEFT JOIN comuna_cl ON propiedades.comuna_ciudad = comuna_cl.id_co) LEFT JOIN negocio ON propiedades.operacion = negocio.id_negocio) LEFT JOIN tipo ON propiedades.tipo = tipo.id_tipo) LEFT JOIN region_cl ON propiedades.region = region_cl.id_re) LEFT JOIN estado ON propiedades.estado = estado.id_estado) LEFT JOIN tramitante ON propiedades.tramitante = tramitante.id_tramitante) LEFT JOIN tipo_tramitante ON tramitante.tipo_tramitante = tipo_tramitante.id_tipo_tramitante WHERE(((negocio.id_negocio) = {0}) AND((tipo.id_tipo) = {1}) AND((region_cl.id_re) = {2}) AND((comuna_cl.id_co) = {3}));", neg, tip, reg, ciu);
+                    MySqlCommand objCmd = new MySqlCommand(strSQL, objConn);
+                    System.Diagnostics.Debug.WriteLine(strSQL);
+                    var dtReader = objCmd.ExecuteReader();
+                    DataList1.DataSource = dtReader;
+                    DataList1.DataBind();
+                    dtReader.Close();
+                    dtReader = null;
+                }
+                else
+                {
+                    String strSQL = String.Format("SELECT propiedades.codigo, negocio.nom_negocio, tipo.nom_tipo, region_cl.str_descripcion, comuna_cl.str_descripcion as comuna_str, estado.str_descripcion as estado_str, propiedades.calle, propiedades.numero, propiedades.villa_condominio, propiedades.habitaciones, propiedades.banos, propiedades.estacionamiento, propiedades.valor_peso, propiedades.valor_uf, tipo_tramitante.nombre_tipo_tramitante, propiedades.fecha, propiedades.vigente FROM((((((propiedades LEFT JOIN comuna_cl ON propiedades.comuna_ciudad = comuna_cl.id_co) LEFT JOIN negocio ON propiedades.operacion = negocio.id_negocio) LEFT JOIN tipo ON propiedades.tipo = tipo.id_tipo) LEFT JOIN region_cl ON propiedades.region = region_cl.id_re) LEFT JOIN estado ON propiedades.estado = estado.id_estado) LEFT JOIN tramitante ON propiedades.tramitante = tramitante.id_tramitante) LEFT JOIN tipo_tramitante ON tramitante.tipo_tramitante = tipo_tramitante.id_tipo_tramitante WHERE(((negocio.id_negocio) = {0}) AND((tipo.id_tipo) = {1}) AND((region_cl.id_re) = {2}));", neg, tip, reg);
+                    MySqlCommand objCmd = new MySqlCommand(strSQL, objConn);
+                    System.Diagnostics.Debug.WriteLine(strSQL);
+                    var dtReader = objCmd.ExecuteReader();
+                    DataList1.DataSource = dtReader;
+                    DataList1.DataBind();
+                    dtReader.Close();
+                    dtReader = null;
+                }
+            }
+            else
+            {
+                String strSQL = String.Format("SELECT propiedades.codigo, negocio.nom_negocio, tipo.nom_tipo, region_cl.str_descripcion, comuna_cl.str_descripcion as comuna_str, estado.str_descripcion as estado_str, propiedades.calle, propiedades.numero, propiedades.villa_condominio, propiedades.habitaciones, propiedades.banos, propiedades.estacionamiento, propiedades.valor_peso, propiedades.valor_uf, tipo_tramitante.nombre_tipo_tramitante, propiedades.fecha, propiedades.vigente FROM((((((propiedades LEFT JOIN comuna_cl ON propiedades.comuna_ciudad = comuna_cl.id_co) LEFT JOIN negocio ON propiedades.operacion = negocio.id_negocio) LEFT JOIN tipo ON propiedades.tipo = tipo.id_tipo) LEFT JOIN region_cl ON propiedades.region = region_cl.id_re) LEFT JOIN estado ON propiedades.estado = estado.id_estado) LEFT JOIN tramitante ON propiedades.tramitante = tramitante.id_tramitante) LEFT JOIN tipo_tramitante ON tramitante.tipo_tramitante = tipo_tramitante.id_tipo_tramitante WHERE(((negocio.id_negocio) = {0}) AND((tipo.id_tipo) = {1}));", neg, tip);
+                MySqlCommand objCmd = new MySqlCommand(strSQL, objConn);
+                System.Diagnostics.Debug.WriteLine(strSQL);
+                var dtReader = objCmd.ExecuteReader();
+                DataList1.DataSource = dtReader;
+                DataList1.DataBind();
+                dtReader.Close();
+                dtReader = null;
+            }
+        }
+        else
+        {
+            String strSQL = String.Format("SELECT propiedades.codigo, negocio.nom_negocio, tipo.nom_tipo, region_cl.str_descripcion, comuna_cl.str_descripcion as comuna_str, estado.str_descripcion as estado_str, propiedades.calle, propiedades.numero, propiedades.villa_condominio, propiedades.habitaciones, propiedades.banos, propiedades.estacionamiento, propiedades.valor_peso, propiedades.valor_uf, tipo_tramitante.nombre_tipo_tramitante, propiedades.fecha, propiedades.vigente FROM((((((propiedades LEFT JOIN comuna_cl ON propiedades.comuna_ciudad = comuna_cl.id_co) LEFT JOIN negocio ON propiedades.operacion = negocio.id_negocio) LEFT JOIN tipo ON propiedades.tipo = tipo.id_tipo) LEFT JOIN region_cl ON propiedades.region = region_cl.id_re) LEFT JOIN estado ON propiedades.estado = estado.id_estado) LEFT JOIN tramitante ON propiedades.tramitante = tramitante.id_tramitante) LEFT JOIN tipo_tramitante ON tramitante.tipo_tramitante = tipo_tramitante.id_tipo_tramitante WHERE(((negocio.id_negocio) = {0}) AND((tipo.id_tipo) = {1}) AND((region_cl.id_re) = {2}) AND((comuna_cl.id_co) = {3}) AND ((propiedades.valor_peso) Between {4} And {5}));", neg, tip, reg, ciu, de, ha);
+            MySqlCommand objCmd = new MySqlCommand(strSQL, objConn);
+            System.Diagnostics.Debug.WriteLine(strSQL);
+            var dtReader = objCmd.ExecuteReader();
+            DataList1.DataSource = dtReader;
+            DataList1.DataBind();
+            dtReader.Close();
+            dtReader = null;
+        }
+
+        objConn.Close();
+
+    }
+
+
+    protected void buscar_btn_Click(object sender, EventArgs e)
+    {
+        String neg = "0", tip = "0", reg = "0", ciu = "0";
+        double de = 0, ha = 0;
+
+        /* VALIDAR LOS CAMPOS ANTES DE BUSCAR */
+        /* tipo de negocio opcion 0: " + negocio.Items[negocio.SelectedIndex].Text; */
+
+        if (negocio.SelectedIndex == 0)
+        {
+            label_botton.Visible = true;
+            req_negocio.Visible = true;
+            label_botton.Text = "ERROR - CAMPOS REQUERIDOS";
+        }
+        else
+        {
+            label_botton.Visible = false;
+            req_negocio.Visible = false;
+            label_botton.Text = "";
+            neg = negocio.Items[negocio.SelectedIndex].Value;
+        }
+
+        if (tipo.Enabled == true)
+        {
+            if (tipo.SelectedIndex == 0)
+            {
+                label_botton.Visible = true;
+                req_tipo.Visible = true;
+                label_botton.Text = "ERROR - CAMPOS REQUERIDOS";
+            }
+            else
+            {
+                label_botton.Visible = false;
+                req_tipo.Visible = false;
+                label_botton.Text = "";
+                tip = tipo.Items[tipo.SelectedIndex].Value;
+            }
+        }
+        /* CARGAR EL LISTADO */
+        reg = region.Items[region.SelectedIndex].Value;
+        ciu = ciudad.Items[ciudad.SelectedIndex].Value;
+
+        /*verificar q sea string el campo desde y hasta*/
+        if (desde.Text.Length > 1)
+        {
+            if (desde.Text != "Desde")
+            {
+                de = Convert.ToDouble(desde.Text);
+            }
+            else
+            {
+                de = 0;
+            }
+        }
+        else
+        {
+            de = 0;
+        }
+
+        if (hasta.Text.Length > 1)
+        {
+            if (hasta.Text != "Hasta")
+            {
+                ha = Convert.ToDouble(hasta.Text);
+            }
+            else
+            {
+                ha = 0;
+            }
+        }
+        else
+        {
+            ha = 0;
+        }
+
+        if (de > ha)
+        {
+            ha = de;
+            label_botton.Visible = true;
+            label_botton.Text = "VALOR 'HASTA' MENOR QUE 'DESDE'";
+        }
+
+
+        System.Diagnostics.Debug.WriteLine("neg:" + neg + " tip:" + tip + " reg:" + reg + " ciu:" + ciu + " de:" + de + " ha:" + ha);
+        if (label_botton.Text.Length < 2)
+        {
+            this.Nueva_tabla(neg, tip, reg, ciu, de, ha);
+        }
+
+
+    }
+}
